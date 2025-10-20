5df4bf543f7f9b746be13e89cdb5ec22106b558b
kaitod007 <pongamerz1412@gmail.com>
2025-10-21 00:25:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -79,11 +79,4 @@ function handleNoClick() {
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
-}
-yesBtn.addEventListener('click', () => {
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 }
-    });
 
